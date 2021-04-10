dca6aee80016a2a25d8be54d5899bded45a0bb0c
Xeros08 <42175313+Xeros08@users.noreply.github.com>
2021-04-10 17:56:17 +0200

    Fixed install script and created update script for Windows

--- Instalation/_install.bat
+++ Instalation/_install.bat
@@ -16,6 +16,6 @@ xcopy .\RimWorld-Spanish-master\Core\* .\Data\Core\Languages\"(GitHub) Spanish"\
 xcopy .\RimWorld-Spanish-master\Royalty\* .\Data\Royalty\Languages\"(GitHub) Spanish"\ /E /Y
 
 :: Delete the uncompressed files
-del .\RimWorld-Spanish-master
+rmdir .\RimWorld-Spanish-master /S /Q
 
 :: End of Batch File
---
+++ Instalation/_update.bat
@@ -0,0 +1,25 @@
+:: Delete the old files
+rmdir .\Data\Core\Languages\"(GitHub) Spanish"  /S /Q
+rmdir .\Data\Royalty\Languages\"(GitHub) Spanish"  /S /Q
+
+:: Download the Repository
+curl -LJO https://github.com/Ludeon/RimWorld-Spanish/archive/refs/heads/master.zip
+
+:: Extract the zip file
+powershell Expand-Archive Rimworld-Spanish-master.zip -DestinationPath .\
+
+:: Delete the old zip file
+del .\RimWorld-Spanish-master.zip
+
+:: Make folders for the languages
+mkdir .\Data\Core\Languages\"(GitHub) Spanish"
+mkdir .\Data\Royalty\Languages\"(GitHub) Spanish"
+
+:: Copy the files to the corresponding directories
+xcopy .\RimWorld-Spanish-master\Core\* .\Data\Core\Languages\"(GitHub) Spanish"\ /E /Y
+xcopy .\RimWorld-Spanish-master\Royalty\* .\Data\Royalty\Languages\"(GitHub) Spanish"\ /E /Y
+
+:: Delete the uncompressed files
+rmdir .\RimWorld-Spanish-master /S /Q
+
+:: End of Batch File
