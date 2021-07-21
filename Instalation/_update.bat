3750de95b29be85c030dc198f295ba6da6912146
Xeros08 <42175313+Xeros08@users.noreply.github.com>
2021-07-21 17:52:33 +0200

    Actualizado script de actualización de traducciones

--- Instalation/_update.bat
+++ Instalation/_update.bat
@@ -14,10 +14,12 @@ del .\RimWorld-Spanish-master.zip
 :: Make folders for the languages
 mkdir .\Data\Core\Languages\"Spanish (GitHub)"
 mkdir .\Data\Royalty\Languages\"Spanish (GitHub)"
+mkdir .\Data\Ideology\Languages\"Spanish (GitHub)"
 
 :: Copy the files to the corresponding directories
 xcopy .\RimWorld-Spanish-master\Core\* .\Data\Core\Languages\"Spanish (GitHub)"\ /E /Y
 xcopy .\RimWorld-Spanish-master\Royalty\* .\Data\Royalty\Languages\"Spanish (GitHub)"\ /E /Y
+xcopy .\RimWorld-Spanish-master\Ideology\* .\Data\Ideology\Languages\"Spanish (GitHub)"\ /E /Y
 
 :: Delete the uncompressed files
 rmdir .\RimWorld-Spanish-master /S /Q
